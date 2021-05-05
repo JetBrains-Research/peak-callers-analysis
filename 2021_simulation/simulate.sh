0aaae41c5452af4797251e602c049a13e0477d5c
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2021-05-05 16:57:35 +0300

    Fix index command

--- 2021_simulation/simulate.sh
+++ 2021_simulation/simulate.sh
@@ -6,7 +6,7 @@ WORK_DIR=/mnt/stripe/shpynov/2021_chips
 for i in {1..20}; do
   echo "Processing chromosome $i"
   samtools faidx $FASTA chr$i > $WORK_DIR/fasta/chr$i.fasta;
-  samtools idx $WORK_DIR/fasta/chr$i.fasta;
+  samtools faidx $WORK_DIR/fasta/chr$i.fasta;
 done;
 
 T=$'\t'
@@ -15,7 +15,7 @@ for PEAKS in encode macs2 sicer span; do
   for M in H3K27ac H3K27me3 H3K36me3 H3K4me1 H3K4me3; do
     MF=$PEAKS_DIR/$PEAKS/$M.json
     PF=$(find $PEAKS_DIR/$PEAKS -name "$M*" | grep -v json | grep -v chr);
-    for i in {1..20}; do
+    for i in {15..16}; do
        PF_CHR=$PF.chr$i;
        cat $PF | grep "chr$i$T" | head -n 1000 > $PF_CHR;
 #      for READS in 1000000 100000 10000000; do
@@ -26,7 +26,7 @@ for PEAKS in encode macs2 sicer span; do
         mkdir -p $OF
         cd $OF
         chips simreads -p $PF_CHR -f $FA -o ${M}_${PEAKS}_chr${i}_1mln -t bed -c 5 --numreads 1000000 \
-          --model $MF --scale-outliers --seed 42 --thread 24 --numcopies 1000;
+          --model $MF --scale-outliers --seed 42 --thread 24;
     done;
   done;
 done;
