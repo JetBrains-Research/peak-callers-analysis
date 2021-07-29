4ae26f7776c03c398801ca53e256e5b00299c1c7
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2021-07-29 12:32:59 +0300

    Compute intersection length

--- 2021_simulation/analyze.sh
+++ 2021_simulation/analyze.sh
@@ -3,13 +3,12 @@ WORK_DIR=/mnt/stripe/shpynov/2021_chips
 N=10
 
 T=$'\t'
-echo "Modification${T}Mult${T}Library${T}I${T}TruePeaksFile${T}TruePeaks${T}TrueLength${T}Tool${T}PeaksFile${T}Fdr${T}Peaks${T}Length${T}Precision${T}Recall" >report.tsv
+echo "Modification${T}Mult${T}Library${T}I${T}TruePeaksFile${T}TruePeaks${T}TrueLength${T}Tool${T}PeaksFile${T}Fdr${T}Peaks${T}Length${T}PrecisionP${T}RecallP${T}Intersection" > report.tsv
 
 for M in H3K27ac H3K27me3 H3K36me3 H3K4me1 H3K4me3; do
   echo "Modification $M"
   for LIB in 200k 500k 1mln; do
     for I in $(seq 1 $N); do
-      echo "Chromosome chr15"
       TPF=$WORK_DIR/fastq/${M}_chr15_${I}.bed
       echo "True peaks file $TPF"
       TL=$(cat $TPF | awk '{L += $3-$2} END {print L}')
@@ -23,14 +22,16 @@ for M in H3K27ac H3K27me3 H3K36me3 H3K4me1 H3K4me3; do
         
         echo "MACS2 narrow"
         for F in $(find $WORK_DIR/macs2/ -name "${NAME}*.narrowPeak"); do
-          echo $F
+          echo $
           P=$(cat $F | wc -l)
           L=$(cat $F | awk '{L+=$3-$2} END {print L}')
           FDR=$(echo $F | sed -E 's/.*_q|_peaks.*//g')
           PR=$(bedtools intersect -a $TPF -b $F -wa -u | wc -l)
           RE=$(bedtools intersect -a $F -b $TPF -wa -u | wc -l)
-          echo "Fdr $FDR Peaks $P Precision $PR Recall $RE"
-          echo "${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}Macs2$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}" >> report.tsv
+          INT=$(bedtools intersect -a $TPF -b $F | awk '{L+=$3-$2} END {print L}')
+          ROW="${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}Macs2$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}$T${INT}"
+          echo $ROW
+          echo "$ROW" >> report.tsv
         done
         echo ""
         
@@ -42,8 +43,10 @@ for M in H3K27ac H3K27me3 H3K36me3 H3K4me1 H3K4me3; do
           FDR=$(echo $F | sed -E 's/.*_broad|_peaks.*//g')
           PR=$(bedtools intersect -a $TPF -b $F -wa -u | wc -l)
           RE=$(bedtools intersect -a $F -b $TPF -wa -u | wc -l)
-          echo "Fdr $FDR Peaks $P Precision $PR Recall $RE"
-          echo "${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}Macs2Broad$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}" >> report.tsv
+          INT=$(bedtools intersect -a $TPF -b $F | awk '{L+=$3-$2} END {print L}')
+          ROW="${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}Macs2Broad$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}$T${INT}"
+          echo $ROW
+          echo "$ROW" >> report.tsv
         done
         echo ""
         
@@ -55,8 +58,10 @@ for M in H3K27ac H3K27me3 H3K36me3 H3K4me1 H3K4me3; do
           FDR=$(echo $F | sed -E 's/.*-FDR//g')
           PR=$(bedtools intersect -a $TPF -b $F -wa -u | wc -l)
           RE=$(bedtools intersect -a $F -b $TPF -wa -u | wc -l)
-          echo "Fdr $FDR Peaks $P Precision $PR Recall $RE"
-          echo "${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}SICER$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}" >> report.tsv
+          INT=$(bedtools intersect -a $TPF -b $F | awk '{L+=$3-$2} END {print L}')
+          ROW="${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}SICER$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}$T${INT}"
+          echo $ROW
+          echo "$ROW" >> report.tsv
         done
         echo ""
 
@@ -68,25 +73,13 @@ for M in H3K27ac H3K27me3 H3K36me3 H3K4me1 H3K4me3; do
           FDR=$(echo $F | sed -E 's/.*200_|_5.peak//g')
           PR=$(bedtools intersect -a $TPF -b $F -wa -u | wc -l)
           RE=$(bedtools intersect -a $F -b $TPF -wa -u | wc -l)
-          echo "Fdr $FDR Peaks $P Precision $PR Recall $RE"
-          echo "${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}SPAN-GAP5$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}" >> report.tsv
+          INT=$(bedtools intersect -a $TPF -b $F | awk '{L+=$3-$2} END {print L}')
+          ROW="${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}SPAN-GAP5$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}$T${INT}"
+          echo $ROW
+          echo "$ROW" >> report.tsv
         done
         echo ""
           
-        echo "SPAN gap 0"
-        for F in $(find $WORK_DIR/span/ -name "${NAME}_*_0.peak"); do
-          echo $F
-          P=$(cat $F | wc -l)
-          L=$(cat $F | awk '{L+=$3-$2} END {print L}')
-          FDR=$(echo $F | sed -E 's/.*200_|_0.peak//g')
-          PR=$(bedtools intersect -a $TPF -b $F -wa -u | wc -l)
-          RE=$(bedtools intersect -a $F -b $TPF -wa -u | wc -l)
-          echo "Fdr $FDR Peaks $P Precision $PR Recall $RE"
-          echo "${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}SPAN-GAP0$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}" >> report.tsv
-        done
-        echo ""
-
-
         echo "SPAN NBHMM2NZ"
         for F in $(find $WORK_DIR/nbhmm2nz/ -name "${NAME}*.peak"); do
           echo $F
@@ -95,76 +88,13 @@ for M in H3K27ac H3K27me3 H3K36me3 H3K4me1 H3K4me3; do
           FDR=$(echo $F | sed -E 's/.*_|\.peak//g')
           PR=$(bedtools intersect -a $TPF -b $F -wa -u | wc -l)
           RE=$(bedtools intersect -a $F -b $TPF -wa -u | wc -l)
-          echo "Fdr $FDR Peaks $P Precision $PR Recall $RE"
-          echo "${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}SPAN-NZ2$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}" >>report.tsv
+          INT=$(bedtools intersect -a $TPF -b $F | awk '{L+=$3-$2} END {print L}')
+          ROW="${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}SPAN-NZ2$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}$T${INT}"
+          echo $ROW
+          echo "$ROW" >> report.tsv
         done
         echo ""
 
-        echo "SPAN NBHMM3NZ"
-        for F in $(find $WORK_DIR/nbhmm3nz/ -name "${NAME}*.peak"); do
-          echo $F
-          P=$(cat $F | wc -l)
-          L=$(cat $F | awk '{L+=$3-$2} END {print L}')
-          FDR=$(echo $F | sed -E 's/.*_|\.peak//g')
-          PR=$(bedtools intersect -a $TPF -b $F -wa -u | wc -l)
-          RE=$(bedtools intersect -a $F -b $TPF -wa -u | wc -l)
-          echo "Fdr $FDR Peaks $P Precision $PR Recall $RE"
-          echo "${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}SPAN-NZ3$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}" >>report.tsv
-        done
-        echo ""
-        
-        echo "SPAN NBHMM4NZ"
-        for F in $(find $WORK_DIR/nbhmm4nz/ -name "${NAME}*.peak"); do
-          echo $F
-          P=$(cat $F | wc -l)
-          L=$(cat $F | awk '{L+=$3-$2} END {print L}')
-          FDR=$(echo $F | sed -E 's/.*_|\.peak//g')
-          PR=$(bedtools intersect -a $TPF -b $F -wa -u | wc -l)
-          RE=$(bedtools intersect -a $F -b $TPF -wa -u | wc -l)
-          echo "Fdr $FDR Peaks $P Precision $PR Recall $RE"
-          echo "${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}SPAN-NZ4$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}" >>report.tsv
-        done
-        echo ""
-
-        echo "SPAN NBHMM3Z"
-        for F in $(find $WORK_DIR/nbhmm3z/ -name "${NAME}*.peak"); do
-          echo $F
-          P=$(cat $F | wc -l)
-          L=$(cat $F | awk '{L+=$3-$2} END {print L}')
-          FDR=$(echo $F | sed -E 's/.*_|\.peak//g')
-          PR=$(bedtools intersect -a $TPF -b $F -wa -u | wc -l)
-          RE=$(bedtools intersect -a $F -b $TPF -wa -u | wc -l)
-          echo "Fdr $FDR Peaks $P Precision $PR Recall $RE"
-          echo "${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}SPAN-Z3$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}" >>report.tsv
-        done
-        echo ""
-
-        echo "SPAN NBHMM4Z"
-        for F in $(find $WORK_DIR/nbhmm4z/ -name "${NAME}*.peak"); do
-          echo $F
-          P=$(cat $F | wc -l)
-          L=$(cat $F | awk '{L+=$3-$2} END {print L}')
-          FDR=$(echo $F | sed -E 's/.*_|\.peak//g')
-          PR=$(bedtools intersect -a $TPF -b $F -wa -u | wc -l)
-          RE=$(bedtools intersect -a $F -b $TPF -wa -u | wc -l)
-          echo "Fdr $FDR Peaks $P Precision $PR Recall $RE"
-          echo "${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}SPAN-Z4$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}" >>report.tsv
-        done
-        echo ""
-
-
-        echo "SPAN Islands"
-        for F in $(find $WORK_DIR/islands/ -name "${NAME}_*.peak"); do
-          echo $F
-          P=$(cat $F | wc -l)
-          L=$(cat $F | awk '{L+=$3-$2} END {print L}')
-          FDR=$(echo $F | sed -E 's/.*_|\.peak//g')
-          PR=$(bedtools intersect -a $TPF -b $F -wa -u | wc -l)
-          RE=$(bedtools intersect -a $F -b $TPF -wa -u | wc -l)
-          echo "Fdr $FDR Peaks $P Precision $PR Recall $RE"
-          echo "${M}${T}$MULT${T}$LIB${T}$I${T}$TPF$T${TP}$T${TL}${T}SPAN-Islands$T$F${T}${FDR}$T${P}$T${L}$T${PR}$T${RE}" >>report.tsv
-        done
-        echo ""
       done
     done
   done
