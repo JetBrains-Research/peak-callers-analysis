e6b753fedcf4a07f85dfd0b1946dbf331e6f3bda
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2025-07-07 14:57:32 +0200

    Added LanceOtron benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -20,13 +20,11 @@ for BAMSF in bams1 bams2 bams3 bams4 bams5; do
 	time snakemake -p -s ~/work/chipseq-smk-pipeline/Snakefile all --use-conda --cores all  --directory $(pwd) --config fastq_dir=$(pwd) start_with_bams=True genome=hg38 homer=True --rerun-incomplete --rerun-triggers mtime;
 	time snakemake -p -s ~/work/chipseq-smk-pipeline/Snakefile all --use-conda --cores all  --directory $(pwd) --config fastq_dir=$(pwd) start_with_bams=True genome=hg38 hotspot=True hotspot_executable=~/data/2025_peak_callers/hotspot-4.1.1/hotspot-distr/hotspot-deploy/bin/hotspot --rerun-incomplete --rerun-triggers mtime;
 	time snakemake -p -s ~/work/chipseq-smk-pipeline/Snakefile all --use-conda --cores all  --directory $(pwd) --config fastq_dir=$(pwd) start_with_bams=True genome=hg38 peakseq=True peakseq_executable=~/data/2025_peak_callers/PeakSeq/bin/PeakSeq --rerun-incomplete --rerun-triggers mtime;
-
-	# Slow
 	time snakemake -p -s ~/work/chipseq-smk-pipeline/Snakefile all --use-conda --cores all  --directory $(pwd) --config fastq_dir=$(pwd) start_with_bams=True genome=hg38 fseq2=True --rerun-incomplete --rerun-triggers mtime;
+	time snakemake -p -s ~/work/chipseq-smk-pipeline/Snakefile all --use-conda --cores all  --directory $(pwd) --config fastq_dir=$(pwd) start_with_bams=True genome=hg38 lanceotron=True --rerun-incomplete --rerun-triggers mtime;
 
 	# Extremely slow, more than 1 hour per sample
 	# time snakemake -p -s ~/work/chipseq-smk-pipeline/Snakefile all --use-conda --cores all  --directory $(pwd) --config fastq_dir=$(pwd) start_with_bams=True genome=hg38 gps=True --rerun-incomplete --rerun-triggers mtime;
 	# time snakemake -p -s ~/work/chipseq-smk-pipeline/Snakefile all --use-conda --cores all  --directory $(pwd) --config fastq_dir=$(pwd) start_with_bams=True genome=hg38 bayespeak=True bayespeak_rscript_executable=~/miniconda3/envs/bayespeak/bin/Rscript --rerun-incomplete --rerun-triggers mtime;
 
-	stop;
 done;
